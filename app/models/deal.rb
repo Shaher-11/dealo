daaee910b6ac9671a8a2c94fa110155cd222dc0a
Shaher-11 <shahershamroukh@gmail.com>
2021-05-24 13:15:50 +0200

    Add validations

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,8 @@
 class Deal < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_and_belongs_to_many :groups, optional: true
+
+  
+  validates :amount, presence: true, numericality: { only_float: true }
+  validates :title, presence: true, length: { minimum: 5, maximum: 30 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :deals
   has_many :groups
-  
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 30 }
 end
