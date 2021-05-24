b0c80307e5a8e0f3627be82dbc342a22c3a53765
Shaher-11 <shahershamroukh@gmail.com>
2021-05-24 12:35:16 +0200

    Set tables association

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,4 @@
 class Deal < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_and_belongs_to_many :groups, optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :deals
+  has_many :groups
+  
 end
