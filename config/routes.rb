0bc2650328dca5a52145d4ba0b2ea7e49b6066e8
Shaher-11 <shahershamroukh@gmail.com>
2021-05-25 10:50:08 +0200

    Add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root 'users#index'
-
-
   get '/sign-up', to: 'users#new'
   get '/sign-in', to: 'sessions#new'
   post '/sign-in', to: 'sessions#create'
   delete '/sign-out', to: 'sessions#destroy'
-
   resources :users, only: %i[index new create show]
-
+  resources :groups, only: %i[index new create show]
+  resources :groups do
+    resources :deals, only: %i[create new]
+  end
+  resources :deals, only: %i[index new create show]
 end
