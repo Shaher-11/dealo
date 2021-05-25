5f445f06ac0f8d6df6fd5155803ef5b2a1994952
Shaher-11 <shahershamroukh@gmail.com>
2021-05-25 13:30:00 +0200

    Add controller and form for creating groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,5 +3,27 @@ class GroupsController < ApplicationController
     @groups = Group.all.order(:name)
   end
 
-  
+  def new
+    @group = Group.new
+  end
+
+  def create
+    user = User.find(current_user.id)
+    @group = user.groups.build(group_params)
+
+    if @group.save
+      flash[:notice] = 'Group was created successfully!'
+      redirect_to @group
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :user)
+  end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -3,8 +3,10 @@ module GroupsHelper
   def make_group(groups)
     html_values = ''
     groups.each do |group|
-      html_values << groups_html_template(group)
+      html_values << group.name
     end
     html_values.html_safe
   end
+
+
 end
